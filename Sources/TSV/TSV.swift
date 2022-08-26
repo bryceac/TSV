f8e8a91e4383c151d8cf32458fe6ac1dc24acfc4
Bryce Campbell <tonyhawk2100@gmail.com>
2022-08-26 15:44:44 -0700

    create the first initializer.

--- Sources/TSV/TSV.swift
+++ Sources/TSV/TSV.swift
@@ -9,5 +9,34 @@ public struct TSV {
 }
 
 extension TSV {
-    init(_ text: String, withHeaders: Bool = false) {}
+    public init(_ text: String, withHeaders: Bool = false) throws {
+        let contents = text.components(separatedBy: .newlines).map { line in
+            line.components(separatedBy: "\t")
+        }
+        
+        let records = withHeaders ? Array(contents.dropFirst()) : contents
+        
+        let longestRow = records.max { firstRow, secondRow in
+            firstRow.count < secondRow.count
+        }!
+        
+        guard withHeaders && contents[0].count >= longestRow.count || !withHeaders && records.allSatisfy({ record in
+            record.count == longestRow.count
+        }) else {
+            let index = records.firstIndex(of: longestRow)!
+            
+            if withHeaders {
+                throw TSVParseError.tooFewColumnHeadings(lineNumber: index+1)
+            } else {
+                throw TSVParseError.columnsNotEqual(lineNumber: index+1)
+            }
+        }
+        
+        if withHeaders {
+            columnHeadings = contents[0]
+            self.records = Matrix(columns: contents[0].count, withGrid: records)
+        } else {
+            self.records = Matrix(withGrid: records)
+        }
+    }
 }
