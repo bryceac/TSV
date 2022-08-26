7509f6e0cf4897d3ab8db451ed43fe8a19684293
Bryce Campbell <tonyhawk2100@gmail.com>
2022-08-26 15:28:17 -0700

    add messages for errors.

--- Sources/TSV/TSVParseError.swift
+++ Sources/TSV/TSVParseError.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+public enum TSVParseError {
+    case tooFewColumnHeadings(lineNumber: Int), columnsNotEqual(lineNumber: Int)
+}
+
+extension TSVParseError: LocalizedError {
+    public var errorDescription: String? {
+        var error: String? = nil
+        
+        switch self {
+        case let .tooFewColumnHeadings(line): error = "The number of headings must equal or exceed the number of columns found on line \(line)."
+        case let .columnsNotEqual(line): error = "All records must have the number of columns as line \(line)."
+        }
+        
+        return error
+    }
+}
