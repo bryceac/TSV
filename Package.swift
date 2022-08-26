cabddf139b3c1c5c74e0053a33f4c845b9b99277
Bryce Campbell <tonyhawk2100@gmail.com>
2022-08-26 15:16:17 -0700

    add package as dependency to help with traversal.

--- Package.swift
+++ Package.swift
@@ -14,13 +14,14 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/bryceac/Matrix", .upToNextMinor(from: "0.3.3"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "TSV",
-            dependencies: []),
+            dependencies: ["Matrix"]),
         .testTarget(
             name: "TSVTests",
             dependencies: ["TSV"]),
