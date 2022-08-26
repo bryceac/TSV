a98528039737316327f06d5591db5f9fc55463f8
Bryce Campbell <tonyhawk2100@gmail.com>
2022-08-26 15:53:18 -0700

    setup first test.

--- Tests/TSVTests/TSVTests.swift
+++ Tests/TSVTests/TSVTests.swift
@@ -2,10 +2,14 @@ import XCTest
 @testable import TSV
 
 final class TSVTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(TSV().text, "Hello, World!")
+    func testTSVParsing() {
+        let text = """
+        Date\tCheck No.\tReconciled\tCategory\tVendor\tMemo\tDeposit\tWithdrawal
+        08/25/2022\t1260\tY\tOpening Balance\tSam Hill Credit Union\tOpen Account\t500
+        08/25/2022\t\tN\tGifts\tFake Street Electronics\tHead set\t\t200
+        08/25/2022\t\tN\t\tVelociraptor Entertainment\tPay Day\t50000
+        """
+        
+        XCTAssertNoThrow(try TSV(text, withHeaders: true))
     }
 }
