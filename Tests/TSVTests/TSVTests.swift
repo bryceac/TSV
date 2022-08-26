9acdc4dfcbb658cb31b6900628522cf08a0fe9ea
Bryce Campbell <tonyhawk2100@gmail.com>
2022-08-26 15:59:51 -0700

    test that initializer fails appropriately.

--- Tests/TSVTests/TSVTests.swift
+++ Tests/TSVTests/TSVTests.swift
@@ -12,4 +12,17 @@ final class TSVTests: XCTestCase {
         
         XCTAssertNoThrow(try TSV(text, withHeaders: true))
     }
+    
+    func testTSVPARsingFailsDueToTooFewHeaders() {
+        let text = """
+        Date\tCheck No.\tReconciled\tCategory\tVendor\tMemo\tDeposit
+        08/25/2022\t1260\tY\tOpening Balance\tSam Hill Credit Union\tOpen Account\t500
+        08/25/2022\t\tN\tGifts\tFake Street Electronics\tHead set\t\t200
+        08/25/2022\t\tN\t\tVelociraptor Entertainment\tPay Day\t50000
+        """
+        
+        XCTAssertThrowsError(try TSV(text, withHeaders: true), "initializer should throw error because there are too few headers") { error in
+            XCTAssertEqual(error as? TSVParseError, TSVParseError.tooFewColumnHeadings(lineNumber: 1))
+        }
+    }
 }
