4707c1f21cb03be818cf8fdd5b04202597722bbc
Bryce Campbell <tonyhawk2100@gmail.com>
2022-08-26 16:04:20 -0700

    add test to make sure columns are equal.

--- Tests/TSVTests/TSVTests.swift
+++ Tests/TSVTests/TSVTests.swift
@@ -25,4 +25,16 @@ final class TSVTests: XCTestCase {
             XCTAssertEqual(error as? TSVParseError, TSVParseError.tooFewColumnHeadings(lineNumber: 2))
         }
     }
+    
+    func testTSVParsingFailsDueToUnequalColumns() {
+        let text = """
+        08/25/2022\t1260\tY\tOpening Balance\tSam Hill Credit Union\tOpen Account\t500
+        08/25/2022\t\tN\tGifts\tFake Street Electronics\tHead set\t\t200
+        08/25/2022\t\tN\t\tVelociraptor Entertainment\tPay Day\t50000
+        """
+        
+        XCTAssertThrowsError(try TSV(text), "initializer should throw error because columns are not equal.") { error in
+            XCTAssertEqual(error as? TSVParseError, TSVParseError.tooFewColumnHeadings(lineNumber: 2))
+        }
+    }
 }
