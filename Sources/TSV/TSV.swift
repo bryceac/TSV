20ff194be29b9570df2e89c58fefd9944833ed36
Bryce Campbell <tonyhawk2100@gmail.com>
2022-08-26 15:21:15 -0700

    setup first initializer.

--- Sources/TSV/TSV.swift
+++ Sources/TSV/TSV.swift
@@ -1,7 +1,13 @@
 import Foundation
 import Matrix
 
+/// A representation of a TSV object
 public struct TSV {
-    public var columns: [String]? = nil
+    /// the headings for the columns
+    public var columnHeadings: [String]? = nil
     public var records: Matrix<String>
+}
+
+extension TSV {
+    init(_ text: String, withHeaders: Bool = false) {}
 }
