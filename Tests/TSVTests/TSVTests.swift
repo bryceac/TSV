2b30ff439c6611da0ef7a42f511b88320148f0bd
Bryce Campbell <tonyhawk2100@gmail.com>
2022-08-26 18:15:20 -0700

    check that string output is right.

--- Tests/TSVTests/TSVTests.swift
+++ Tests/TSVTests/TSVTests.swift
@@ -250,4 +250,17 @@ final class TSVTests: XCTestCase {
         
         XCTAssertEqual(tsv[0,0], newDate)
     }
+    
+    func testTSVStringEqualsOriginal() throws {
+        let text = """
+        Date\tCheck No.\tReconciled\tCategory\tVendor\tMemo\tDeposit\tWithdrawal
+        08/25/2022\t1260\tY\tOpening Balance\tSam Hill Credit Union\tOpen Account\t500
+        08/25/2022\t\tN\tGifts\tFake Street Electronics\tHead set\t\t200
+        08/25/2022\t\tN\t\tVelociraptor Entertainment\tPay Day\t50000
+        """
+        
+        let tsv = try TSV(text, withHeaders: true)
+        
+        XCTAssertEqual("\(tsv)", text)
+    }
 }
