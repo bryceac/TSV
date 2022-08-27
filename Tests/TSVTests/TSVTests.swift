3cdd80b05396295b1a8ced1c59dfaa633f0406ba
Bryce Campbell <tonyhawk2100@gmail.com>
2022-08-26 17:42:48 -0700

    finish putting in more tests for subscripts.

--- Tests/TSVTests/TSVTests.swift
+++ Tests/TSVTests/TSVTests.swift
@@ -218,4 +218,19 @@ final class TSVTests: XCTestCase {
         
         XCTAssertEqual(tsv[0,0], "08/25/2022")
     }
+    
+    func testRetrieveColumnByName() throws {
+        let text = """
+        Date\tCheck No.\tReconciled\tCategory\tVendor\tMemo\tDeposit\tWithdrawal
+        08/25/2022\t1260\tY\tOpening Balance\tSam Hill Credit Union\tOpen Account\t500
+        08/25/2022\t\tN\tGifts\tFake Street Electronics\tHead set\t\t200
+        08/25/2022\t\tN\t\tVelociraptor Entertainment\tPay Day\t50000
+        """
+        
+        let tsv = try TSV(text, withHeaders: true)
+        
+        let expectedColumn = [String](repeating: "08/25/2022", count: 3)
+        
+        XCTAssertEqual(tsv[column: "Date"], expectedColumn)
+    }
 }
